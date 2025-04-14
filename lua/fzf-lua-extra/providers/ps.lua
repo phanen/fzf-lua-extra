6480de0ac90882411d2455833c87689046a1b2f7
phanium <91544758+phanen@users.noreply.github.com>
2025-04-14 18:34:07 +0800

    feat(ps): colorize columns

--- lua/fzf-lua-extra/providers/ps.lua
+++ lua/fzf-lua-extra/providers/ps.lua
@@ -14,31 +14,53 @@ return function(opts)
     return
   end
 
-  -- local exec_lua = function(_) return _ end
+  local exec_lua = function(_) return _ end
   opts = vim.tbl_deep_extend('force', {
     cmd = cmd,
     ps_preview_cmd = 'ps --no-headers -wwo cmd',
-    -- requires_processing = true,
-    -- multiprocess = true,
-    -- can use TSInjector for bash line maybe
-    -- __mt_transform = exec_lua [[return function(e) return e end]],
+    requires_processing = true,
+    -- debug = true,
+    multiprocess = true,
+    __mt_preprocess = exec_lua [[
+      local utils = require('fzf-lua.utils')
+      _G.bold = utils.ansi_codes.bold
+      _G.blue = utils.ansi_codes.blue
+      _G.green = utils.ansi_codes.green
+      _G.magenta = utils.ansi_codes.magenta
+      _G.hl_cmd = function(cmd)
+        return cmd
+      end
+    ]],
+    __mt_transform = exec_lua [[
+      return function(e)
+        if e:match('^%s*PID') then
+          local sep1, pid, sep2, ppid, sep3, cmd = e:match('^(%s*)(%S+)(%s*)(%S+)(%s*)(%S+)$')
+          return ('%s%s%s%s%s%s'):format(sep1, bold(pid), sep2, bold(ppid), sep3, bold(cmd))
+        end
+        local sep1, pid, sep2, ppid, sep3, cmd = e:match('^(%s*)(%d+)(%s*)(%d+)(%s*)(.*)$')
+        return ('%s%s%s%s%s%s'):format(sep1, magenta(pid), sep2, blue(ppid), sep3, hl_cmd(cmd))
+      end
+    ]],
     fzf_opts = {
       ['--ansi'] = true,
       ['--header-lines'] = 1,
-      ['--color'] = 'fg:dim,nth:regular',
       ['--multi'] = true,
       ['--no-multi'] = false,
       -- ['--nth'] = '-1',
       -- ['--track'] = true,
+      -- ['--no-hscroll'] = true,
+    },
+    fzf_colors = {
+      ['fg'] = 'dim',
+      ['nth'] = 'regular',
+      ['hl+'] = '-1:reverse',
+      ['hl'] = '-1:reverse',
     },
     keymap = {
       fzf = {
         ['click-header'] = utils.fzf_version()[2] > 0.59
             and [[transform-nth(echo "$FZF_CLICK_HEADER_NTH")+transform-prompt(echo "$FZF_CLICK_HEADER_WORD> ")]]
           or nil,
-        -- cursorhold? top? https://github.com/junegunn/fzf/issues/1211
-        ['ctrl-r'] = ('reload:%s'):format(cmd),
-        change = ('reload:%s'):format(cmd),
       },
     },
     previewer = {
@@ -49,6 +71,7 @@ return function(opts)
           o = o or {}
           local act = shell.raw_preview_action_cmd(function(items)
             local pid = (items[1]):match('^%s*(%d+)')
+            if not pid then return 'echo no preview' end
             return opts.ps_preview_cmd .. ' ' .. pid
           end, '{}', self.opts.debug)
           return act
@@ -64,6 +87,9 @@ return function(opts)
       -- treesitter = true,
     },
     actions = {
+      -- cursorhold? top? https://github.com/junegunn/fzf/issues/1211
+      ['ctrl-r'] = { fn = function() end, reload = true },
+      change = { fn = function() end, reload = true },
       ['ctrl-x'] = {
         fn = function(selected)
           local pids = vim.tbl_map(function(s) return tonumber(s:match('^%s*(%d+)')) end, selected)
