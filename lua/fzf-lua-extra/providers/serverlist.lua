d84d0df635ae928049ad877c8278d6caa921615c
phanium <91544758+phanen@users.noreply.github.com>
2025-10-21 18:08:16 +0800

    fix(serverlist): refresh live server list when possilbe
    Maybe boardcast to peers on die... but sigkill is uncatchable.

--- lua/fzf-lua-extra/providers/serverlist.lua
+++ lua/fzf-lua-extra/providers/serverlist.lua
@@ -3,17 +3,19 @@ return function(opts)
   vim.fn.system({ 'touch', scrshot })
   local default = {
     previewer = {
-      cmd = [[nvim --clean --headless --remote-expr 'nvim__screenshot("/tmp/screenshot")' --server {}; cat /tmp/screenshot]],
+      cmd = [[nvim --clean --headless --remote-expr 'nvim__screenshot("/tmp/screenshot")' --server {} && cat /tmp/screenshot]],
       _ctor = require('fzf-lua.previewer').fzf.cmd,
     },
     actions = { ['enter'] = vim.schedule_wrap(function(sel) vim.cmd.connect(sel[1]) end) },
   }
   opts = vim.tbl_deep_extend('force', default, opts or {})
-  local list = vim
-    .iter(vim.fn.serverlist({ peer = true }))
-    :filter(
-      function(e) return not e:match('fzf%-lua') and not vim.tbl_contains(vim.fn.serverlist(), e) end
-    )
-    :totable()
-  require('fzf-lua').fzf_exec(list, opts)
+  opts._resume_reload = true -- avoid list contain killed server unhide
+  require('fzf-lua').fzf_exec(function(cb)
+    vim
+      .iter(vim.fn.serverlist({ peer = true }))
+      :filter(
+        function(e) return not e:match('fzf%-lua') and not vim.tbl_contains(vim.fn.serverlist(), e) end
+      )
+      :each(cb)
+  end, opts)
 end
