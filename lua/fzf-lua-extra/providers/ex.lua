ae39760e53531d30b8618e6a53b16b26e7e051f3
phanium <91544758+phanen@users.noreply.github.com>
2025-10-23 21:19:08 +0800

    feat: ex cmd mvp
    Have to steal codes in blink.cmp to make it more usable
    (e.g. don't work with ignorecase/smartcase)
    
    Maybe minibuffer can be easliy reinvented in vim in future with extui

---
+++ lua/fzf-lua-extra/providers/ex.lua
@@ -0,0 +1,25 @@
+local __DEFAULT__ = {
+  preview = 'true',
+  line_query = function(q) return nil, (q:match('%S+$') or '') end,
+  winopts = { preview = { hidden = true } },
+  keymap = {
+    fzf = {
+      start = 'toggle-search',
+      tab = 'transform:' .. FzfLua.shell.stringify_data(
+        function(s, _, _) return 'change-query:' .. (s[1]:gsub('%S+$', s[2])) end,
+        {},
+        '{q} {}'
+      ),
+    },
+  },
+  actions = {
+    enter = { fn = function(s) vim.cmd(s[1]) end, field_index = '{q}' },
+  },
+}
+
+return function(opts)
+  assert(__DEFAULT__)
+  require('fzf-lua').fzf_live(function(s)
+    return function(cb) vim.iter(vim.fn.getcompletion(s[1], 'cmdline')):each(cb) end
+  end, opts)
+end
