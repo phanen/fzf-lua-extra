2834f985ff3ce4c98cae8bcaf861b27b10f8bbfb
phanium <91544758+phanen@users.noreply.github.com>
2025-11-21 23:43:25 +0800

    feat: poor man http

--- lua/fzf-lua-extra/providers/cmd.lua
+++ lua/fzf-lua-extra/providers/cmd.lua
@@ -1,18 +1,29 @@
+local c
 ---@class fle.config.Cmd: fzf-lua.config.Base
 local __DEFAULT__ = {
   allow = {
     journalctl = true,
+    curl = true,
   },
   fzf_opts = {
     ['--raw'] = true,
   },
+  preview = {
+    fn = function(...) return c(...) end,
+    type = 'cmd',
+    field_index = '{q}',
+  },
 }
+
+c = function(s, opts)
+  -- TODO: callback should have way to access cmd
+  opts = type(opts) == 'table' and opts or __DEFAULT__
+  local q = s[1]
+  local cmd = q:match('^%s*(%S+)')
+  if not cmd or not opts.allow[cmd] then return vim.tbl_keys(opts.allow) end
+  return q
+end
 return function(opts)
   assert(__DEFAULT__)
-  FzfLua.fzf_live(function(s)
-    local q = s[1]
-    local cmd = q:match('^%s*(%S+)')
-    if not cmd or not opts.allow[cmd] then return vim.tbl_keys(opts.allow) end
-    return q
-  end, opts)
+  FzfLua.fzf_live(c, opts)
 end
