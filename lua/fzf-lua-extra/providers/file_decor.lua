7cb5658d2a32d95818b63ded2dfb81ec8eab99fd
phanium <91544758+phanen@users.noreply.github.com>
2026-01-16 19:00:58 +0800

    fix: simple ephemeral

--- lua/fzf-lua-extra/providers/file_decor.lua
+++ lua/fzf-lua-extra/providers/file_decor.lua
@@ -1,5 +1,4 @@
 local ns = vim.api.nvim_create_namespace('fzf-lua-extra.decor')
-local lmarks = {} ---@type [integer, string, string][] -- line number -> { extmark id, icon, hl }
 
 local overlay = true
 
@@ -15,44 +14,35 @@ local __DEFAULT__ = {
   winopts = {
     on_create = function(e)
       vim.api.nvim_set_decoration_provider(ns, {
+        on_win = function(_, win)
+          if e.winid ~= win then return false end
+        end,
         on_line = function(_, _, buf, lnum)
           -- skip prompt 0, maybe not the first line though
-          if buf ~= e.bufnr or lnum == 0 then return end
+          if lnum == 0 then return end
           local line = vim.api.nvim_buf_get_lines(buf, lnum, lnum + 1, true)[1]
           if not line then return end
           local content = line:match('^%s*(%S+)')
-          if not content then
-            if lmarks[lnum] then
-              vim.api.nvim_buf_del_extmark(buf, ns, lmarks[lnum][1])
-              lmarks[lnum] = nil
-            end
-            return
-          end
+          if not content then return end
           -- FIXME: scroll...
           ---@type string?, string?
           local icon, hl = require('mini.icons').get('file', content)
-          if icon and (not lmarks[lnum] or (lmarks[lnum][2] ~= icon and lmarks[lnum][3] ~= hl)) then
-            local id = vim.api.nvim_buf_set_extmark(buf, ns, lnum, 0, {
-              id = (lmarks[lnum] or {})[1],
-              -- virt_text_pos = 'inline', -- this break fzf match hl
-              virt_text_pos = overlay and 'overlay' or 'right_align',
-              virt_text = { { icon .. ' ', hl or 'Error' } },
-            })
-            lmarks[lnum] = lmarks[lnum] or {}
-            lmarks[lnum][1] = id
-            lmarks[lnum][2] = icon
-            lmarks[lnum][3] = hl
-          end
+          if not icon then return end
+          vim.api.nvim_buf_set_extmark(buf, ns, lnum, 0, {
+            -- virt_text_pos = 'inline', -- this break fzf match hl
+            virt_text_pos = overlay and 'overlay' or 'right_align',
+            virt_text = { { icon .. ' ', hl or 'Error' } },
+            ephemeral = true,
+          })
         end,
       })
     end,
-    on_close = function() vim.api.nvim_set_decoration_provider(ns, { on_line = nil }) end,
+    on_close = function() vim.api.nvim_set_decoration_provider(ns, {}) end,
   },
 }
 
 return function(opts)
   assert(__DEFAULT__)
-  lmarks = {}
   -- require('fzf-lua.devicons').load()
   FzfLua.fzf_exec(opts.cmd, opts)
 end
